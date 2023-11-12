e4abd248c8e60885512c343fad66aa6bfbb3c1f2
kita <r.kita@remodela.jp>
2023-11-12 17:02:39 +0900

    FallbackComponentにMessageを使う例

--- src/components/YesOrNo.tsx
+++ src/components/YesOrNo.tsx
@@ -1,7 +1,7 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 const fetchYesOrNo = async (): Promise<string> => {
-  const response = await fetch("https://yesno.wtf/api");
+  const response = await fetch("https://yesno.wtf/api123");
   const data = await response.json();
   return data.answer;
 };
@@ -12,9 +12,6 @@ const YesOrNo = () => {
     queryFn: fetchYesOrNo,
     retry: false,
   });
-
-  // if (isLoading) return <p>Loading 2...</p>;
-  // if (error) return <p>Error occurred!</p>;
 
   return <p>{answer}</p>;
 };
--- src/pages/Page4.tsx
+++ src/pages/Page4.tsx
@@ -1,12 +1,32 @@
-import { Suspense } from 'react';
-import { ErrorBoundary } from "react-error-boundary";
+import { Suspense, useEffect } from 'react';
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import YesOrNo from "../components/YesOrNo";
+import { message } from 'antd';
+
+const ErrorMessage = ({ error }: FallbackProps) => {
+  const [messageApi, contextHolder] = message.useMessage();
+  
+  useEffect(() => {
+    messageApi.error(error.message);
+  }, [error, messageApi]);
+
+  return (
+    <>
+      {contextHolder}
+    </>
+  );
+};
+
+const onError = (error: Error, info: { componentStack: string }) => {
+  console.log('error.message', error.message)
+  console.log('info.componentStack:', info.componentStack)
+}
 
 const Page4 = () => {
   return (
     <>
       <h1>Yes or No?</h1>
-      <ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <ErrorBoundary onError={onError} FallbackComponent={ErrorMessage}>
         <Suspense fallback={<p>Loading 1...</p>}>
           <YesOrNo />
         </Suspense>
