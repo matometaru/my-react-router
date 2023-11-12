9e2f3cada91d42105b0ed29996520d73e273f36f
kita <r.kita@remodela.jp>
2023-11-12 17:22:28 +0900

    a

--- src/components/YesOrNo.tsx
+++ src/components/YesOrNo.tsx
@@ -1,19 +1,41 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { useErrorHandler } from 'react-error-boundary'
 
 const fetchYesOrNo = async (): Promise<string> => {
+  const response = await fetch("https://yesno.wtf/api");
+  const data = await response.json();
+  return data.answer;
+};
+
+const fetchYesOrNoError = async (): Promise<string> => {
   const response = await fetch("https://yesno.wtf/api123");
   const data = await response.json();
   return data.answer;
 };
 
 const YesOrNo = () => {
+  const handleError = useErrorHandler()
+
   const { data: answer } = useSuspenseQuery({
     queryKey: ['yesOrNo'],
     queryFn: fetchYesOrNo,
     retry: false,
   });
 
-  return <p>{answer}</p>;
+  const handleClicked = async () => {
+    try {
+      await fetchYesOrNoError()
+    } catch (error) {
+      handleError(error)
+    }
+  }
+
+  return (
+    <>
+      <p>{answer}</p>
+      <button onClick={handleClicked}>click</button>
+    </>
+  );
 };
 
 export default YesOrNo;
