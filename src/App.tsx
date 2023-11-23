43c5184222dfd2b952ed76ddac5a5018c8fa6e6b
kita <r.kita@remodela.jp>
2023-11-23 16:53:52 +0900

    color設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,24 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router'
 
+import { ConfigProvider } from 'antd';
 import { LoadingProvider } from './context/LoadingContext';
 
 function App() {
   return (
     <div className="App">
-      <LoadingProvider>
-        <RouterProvider router={router} />
-      </LoadingProvider>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#10692A',
+            colorLink: '#10692A',
+          },
+        }}
+      >
+        <LoadingProvider>
+          <RouterProvider router={router} />
+        </LoadingProvider>
+      </ConfigProvider>
     </div>
   );
 }
